b3b16d0a126c3fb9874fbeb424c833c097c73e5a
nguyenducdinh2701 <dinhnd@vieted.com>
2021-08-21 11:43:37 +0700

    d

--- src/Api.js
+++ src/Api.js
@@ -36,6 +36,35 @@ const getCourseComing = async (payload = {}) => {
     }
 }
 
+const apiGetLessionOver = async (payload = {}) => {
+    let fd = new FormData();
+    fd.append('is_teacher',1)
+    fd.append('items_per_page',5)
+    fd.append('_sand_expand','scheduled')
+    fd.append('_sand_ajax',1)
+    fd.append('_sand_platform',3)
+    fd.append('_sand_domain','hth')
+    fd.append('_sand_readmin',1)
+    fd.append('_sand_is_wan',false)
+    fd.append('_sand_token','7a384_60ADY')
+    fd.append('_sand_uiid',267365)
+    fd.append('_sand_uid','5eeae94062f9a8551863e1f4')
+    fd.append('session_status',3)
+    try{
+        const res = await axiosInstance.post(
+            `/session/search`, fd
+        )
+        if(!res?.data.success){
+            throw 'loi he thong'
+        }
+        console.log('res?.data', res?.data)
+        return res?.data
+    }catch (e) {
+        throw e
+    }
+}
+
 export {
-    getCourseComing
+    getCourseComing,
+    apiGetLessionOver
 }
